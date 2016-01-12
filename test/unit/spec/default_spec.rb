51696a9dd8baa005b85d75ff80249b1324f8381a
Michael Burns <michael.burns@rackspace.com>
2016-01-12 00:44:29 -0800

    add unit tests
    Remove useless fixture

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -4,5 +4,13 @@ require_relative 'spec_helper'
 describe 'ele-apt::default' do
   before { stub_resources }
 
-  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new do |node|
+      node.set['lsb']['codename'] = 'trusty'
+    end.converge(described_recipe)
+  end
+
+  # it 'adds an apt repository' do
+  #   expect(chef_run).to create_apt_repository('intapt')
+  # end
 end
