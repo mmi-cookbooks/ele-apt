f2707e3c64699d57a889f84401b7082a8548b953
Michael Burns <michael.burns@rackspace.com>
2016-01-13 00:58:34 -0800

    improve tests, aptly logic
    pass in string, not array of string(s)

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -7,16 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.set['aptly']['uri'] = 'http://intapt.cm.k1k.me/'
+# node.set['aptly']['ppadistributorid'] = "Ubuntu"
+# node.set['aptly']['uri'] = 'http://intapt.cm.k1k.me/'
 node.set['aptly']['dist'] = 'trusty'
 
-include_recipe 'aptly'
+include_recipe 'aptly::install'
 
-directory '/data/aptly' do
+packagedir = '/data/packages'
+
+directory packagedir do
   recursive true
+  owner node['aptly']['user']
+  group node['aptly']['group']
 end
 
 aptly_repo 'aptly' do
   action :add
-  directory '/data/aptly'
+  comment 'Custom packages for MAAS'
+  directory packagedir
+  component node['aptly']['components'].first
+  distribution node['aptly']['dist']
 end
+
+# apache virtualhost
--- test/unit/spec/repo_spec.rb
+++ test/unit/spec/repo_spec.rb
@@ -6,11 +6,15 @@ describe 'ele-apt::repo' do
 
   let(:chef_run) { ChefSpec::SoloRunner.new(step_into: ['aptly']).converge(described_recipe) }
 
-  it 'creates a directory with an explicit action' do
-    expect(chef_run).to create_directory('/data/aptly')
+  it 'includes the `aptly::install` recipe' do
+    expect(chef_run).to include_recipe('aptly::install')
   end
 
-  it 'creates a aptly repository' do
+  it 'creates a directory with an explicit action' do
+    expect(chef_run).to create_directory('/opt/aptly')
+  end
+
+  it 'creates an aptly repository' do
     expect(chef_run).to add_aptly_repo('aptly')
   end
 end
