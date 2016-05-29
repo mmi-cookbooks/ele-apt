4a8e709a5dc67820ca5a0e3b0ae23749ebb06af7
Michael Burns <michael.burns@rackspace.com>
2016-05-29 00:03:05 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures ele-apt'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.2.2'
 
 recipe 'ele-apt', 'Installs ele apt repo'
 recipe 'repo', 'Installs Aptly package repository'
